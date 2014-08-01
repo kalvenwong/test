fd7068b809867d08780282c606b799811fb4949d
细潮 <kalven@outlook.com>
2014-08-01 15:26:55 +0800

    finsh intelnation language and yanzheng

---
+++ houserent_str_hib/src/com/action/LanguageAction.java
@@ -0,0 +1,27 @@
+package com.action;
+
+import java.util.Locale;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LanguageAction extends ActionSupport {
+	
+	public String chinese() {
+		ActionContext.getContext().setLocale(Locale.SIMPLIFIED_CHINESE);
+		ServletActionContext
+				.getRequest()
+				.getSession()
+				.setAttribute("WW_TRANS_I18N_LOCALE", Locale.SIMPLIFIED_CHINESE);
+		return SUCCESS;
+	}
+
+	public String englise(){
+		ActionContext.getContext().setLocale(Locale.US);
+		ServletActionContext.getRequest().getSession().setAttribute("WW_TRANS_I18N_LOCALE",Locale.US);
+		return SUCCESS;
+		}
+	
+}
